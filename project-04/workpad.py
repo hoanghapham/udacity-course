4b0d081def0f960759e58c82ad213cd17d9d50f4
hoanghapham <hoangha.20021992@gmail.com>
2022-12-07 22:50:09 +0700

    update workpad with ETL logic

--- project-04/workpad.py
+++ project-04/workpad.py
@@ -3,8 +3,9 @@ import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import udf, col, monotonically_increasing_id
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql.types import TimestampType
 import pathlib
 
 #%%
@@ -55,11 +56,58 @@ song_df = spark.read.json(song_files)
 song_tbl = song_df.select('song_id', 'title', 'artist_id', 'year', 'duration')
 
 # %%
-song_tbl.write.parquet(project_folder + "/tables/songs.parquet")
+song_tbl.write.parquet(project_folder + "/tables/songs.parquet", mode = 'overwrite')
 # %%
 
 artist_tbl = song_df.select('artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude')
 
 # %%
-artist_tbl.write.parquet(project_folder + "/tables/artists.parquet")
+artist_tbl.write.parquet(project_folder + "tables/artists.parquet", mode = 'overwrite')
+
+# %%
+users_tbl = log_df \
+    .select('userId', 'firstName', 'lastName', 'gender', 'level') \
+    .withColumnRenamed('userId', 'user_id') \
+    .withColumnRenamed('firstName', 'first_name') \
+    .withColumnRenamed('lastName', 'last_name') \
+    .dropDuplicates()
+
+# %%
+users_tbl.write.parquet(project_folder + "/tables/users.parquet", mode = 'overwrite')
+
+# %%
+
+def to_timestamp(timestamp:int) -> datetime:
+    return datetime.utcfromtimestamp(timestamp / 1e3)
+
+udf_to_timestamp = udf(lambda x: to_timestamp(x), TimestampType())
+
+time_tbl = log_df.select('ts') \
+    .withColumn('start_time', udf_to_timestamp(col('ts'))) \
+    .withColumn('hour', hour(col('start_time'))) \
+    .withColumn('day', dayofmonth(col('start_time'))) \
+    .withColumn('week', weekofyear(col('start_time'))) \
+    .withColumn('month', month(col('start_time'))) \
+    .withColumn('year', year(col('start_time'))) \
+    .withColumn('weekday', date_format(col('start_time'), 'E'))
+
+# %%
+
+time_tbl.write.partitionBy('year', 'month').parquet(project_folder + "/tables/time.parquet", mode = 'overwrite')
+
+# %%
+# songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
+
+songplays_tbl = log_df \
+    .join(song_tbl.select('title', 'song_id', 'artist_id'), song_tbl.title == log_df.song, 'left') \
+    .join(time_tbl, log_df.ts == time_tbl.ts, 'left') \
+    .withColumn('songplay_id', monotonically_increasing_id()) \
+    .select('songplay_id', 'start_time', 'year', 'month', 'userId', 'level', 'song_id', 'artist_id', 'sessionId', 'location', 'userAgent') \
+    .withColumnRenamed('userId', 'user_id') \
+    .withColumnRenamed('sessionId', 'session_id') \
+    .withColumnRenamed('userAgent', 'user_agent')
+
+# %%
+
+songplays_tbl.write.partitionBy('year', 'month').parquet(project_folder + "/tables/songplays.parquet", mode = 'overwrite')
 # %%
