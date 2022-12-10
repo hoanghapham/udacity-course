4f2cf691f17a110bc96e3e91621985ae989d209c
hoanghapham <hoangha.20021992@gmail.com>
2022-12-10 13:51:24 +0700

    add makefile & update create_emr script

--- project-04/scripts/create_emr.sh
+++ project-04/scripts/create_emr.sh
@@ -1,5 +1,5 @@
 aws emr create-cluster \
-    --name udacity-20221208 \
+    --name emr-$(date +%s) \
     --release-label emr-5.36.0  \
     --use-default-roles \
     --instance-count 3 \
@@ -11,6 +11,6 @@ aws emr create-cluster \
         Name=JupyterHub \
         Name=JupyterEnterpriseGateway \
         Name=Ganglia \
-    --ec2-attributes KeyName=udacity-spark-asus,SubnetId=subnet-08974a26617d9eb1e \
+    --ec2-attributes KeyName=$(AWS_KEY_NAME),SubnetId=$(AWS_SUBNET_ID) \
     --instance-type m5.xlarge \
     --auto-termination-policy IdleTimeout=2000
