cb3d550f813a1cf87a8950defeff5f401c697e02
hoanghapham <hoangha.20021992@gmail.com>
2023-01-13 21:59:28 +0700

    add queries

--- project-05/plugins/helpers/sql_queries.py
+++ project-05/plugins/helpers/sql_queries.py
@@ -42,23 +42,128 @@ class InsertQueries:
     """)
 
 class StageQueries:
-    staging_events_copy = ("""
-        COPY staging.events
-        FROM '{source_data}'
-        CREDENTIALS 'aws_iam_role={aws_iam_role}'
-        FORMAT AS JSON '{log_jsonpath}'
-        """)
-
-    staging_songs_copy = ("""
-        COPY staging.songs
-        FROM '{source_data}'
-        CREDENTIALS 'aws_iam_role={aws_iam_role}'
-        FORMAT AS JSON 'auto'
-        """)
-
     stage_table = """
         COPY staging.{table}
         FROM '{source_data}'
         CREDENTIALS 'aws_iam_role={aws_iam_role}'
         FORMAT AS JSON 'auto'
     """
+
+class CreateSchemaQueries:
+    staging_schema_create = "CREATE SCHEMA IF NOT EXISTS staging" 
+    analytics_schema_create = "CREATE SCHEMA IF NOT EXISTS analytics" 
+class CreateTableQueries:
+
+    # CREATE TABLES
+
+    staging_events_table_create= ("""
+    CREATE TABLE staging.log (
+        artist              VARCHAR,
+        auth                VARCHAR(20),
+        firstName           VARCHAR,
+        gender              VARCHAR(2),
+        itemInSession       INTEGER,
+        lastName            VARCHAR,
+        length              NUMERIC,
+        level               VARCHAR(10),
+        location            VARCHAR,
+        method              VARCHAR(10),
+        page                VARCHAR,
+        registration        NUMERIC,
+        sessionId           INTEGER,
+        song                VARCHAR,
+        status              INTEGER,
+        ts                  BIGINT,
+        userAgent           VARCHAR(max),
+        userId              INTEGER
+    )
+    """
+    )
+
+    staging_songs_table_create = ("""
+    CREATE TABLE staging.songs (
+        artist_id           VARCHAR,
+        artist_latitude     NUMERIC,
+        artist_location     VARCHAR,
+        artist_longitude    NUMERIC,
+        artist_name         VARCHAR,
+        duration            NUMERIC,
+        num_songs           INTEGER,
+        song_id             VARCHAR PRIMARY KEY,
+        title               VARCHAR,
+        year                INTEGER
+    )
+    """)
+
+    songplays_table_create = ("""
+    CREATE TABLE analytics.songplays (
+        songplay_id         INTEGER IDENTITY(0, 1), 
+        ts                  BIGINT,
+        start_time          TIMESTAMP, 
+        user_id             INTEGER, 
+        level               VARCHAR(10), 
+        song_id             VARCHAR, 
+        artist_id           VARCHAR, 
+        session_id          INTEGER, 
+        location            VARCHAR, 
+        users_agent         VARCHAR(max),
+
+        FOREIGN KEY (ts) REFERENCES analytics.times (ts),
+        FOREIGN KEY (user_id) REFERENCES analytics.users (user_id),
+        FOREIGN KEY (artist_id) REFERENCES analytics.artists (artist_id),
+        FOREIGN KEY (song_id) REFERENCES analytics.songs (song_id)
+    )
+
+    DISTKEY(ts)
+    SORTKEY(ts)
+    """)
+
+    users_table_create = ("""
+    CREATE TABLE analytics.users (
+        user_id             INTEGER UNIQUE PRIMARY KEY, 
+        first_name          VARCHAR, 
+        last_name           VARCHAR, 
+        gender              VARCHAR(2), 
+        level               VARCHAR(10)
+    )
+    DISTSTYLE ALL
+    """)
+
+    songs_table_create = ("""
+    CREATE TABLE analytics.songs (
+        song_id             VARCHAR UNIQUE PRIMARY KEY, 
+        title               VARCHAR, 
+        artist_id           VARCHAR, 
+        year                INTEGER, 
+        duration            NUMERIC,
+        
+        FOREIGN KEY (artist_id) REFERENCES analytics.artists (artist_id)
+    )
+    DISTSTYLE ALL
+    """)
+
+    artists_table_create = ("""
+    CREATE TABLE analytics.artists (
+        artist_id           VARCHAR UNIQUE PRIMARY KEY, 
+        name                VARCHAR, 
+        location            VARCHAR, 
+        lattitude           NUMERIC, 
+        longitude           NUMERIC
+    )
+    DISTSTYLE ALL
+    """)
+
+    time_table_create = ("""
+    CREATE TABLE analytics.times (
+        ts                  BIGINT UNIQUE PRIMARY KEY, 
+        start_time          TIMESTAMP,
+        date                DATE,    
+        hour                INTEGER, 
+        day                 INTEGER, 
+        week                INTEGER, 
+        month               INTEGER, 
+        year                INTEGER, 
+        weekday             INTEGER
+    )
+    DISTSTYLE ALL
+    """)
