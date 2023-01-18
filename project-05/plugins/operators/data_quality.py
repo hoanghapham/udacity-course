3b538e9ed1887d0221171840b376f3446b894974
hoanghapham <hoangha.20021992@gmail.com>
2023-01-18 21:09:54 +0700

    update data quality operator

--- project-05/plugins/operators/data_quality.py
+++ project-05/plugins/operators/data_quality.py
@@ -2,7 +2,7 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from helpers.custom_logger import init_logger
-from helpers.sql_queries import DataQualityQueries
+from helpers.data_tests import CheckHasData, CheckUnique, CheckNotNull
 
 logger = init_logger(__file__)
 
@@ -21,13 +21,54 @@ class DataQualityOperator(BaseOperator):
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        result = redshift_hook.get_records(self.sql.format(table=self.table, column=self.column))
         
-        
+        # Test unique, not null on unique keys of all tables
+        # Test has_row for all tables
 
-        if len(result) > 0 or len(result[0]) > 0:
-            logger.error(f"Data quality check failed. Quality check query: {self.sql}")
-            raise ValueError("Data quality check failed.")
+        artists_tests = [
+            CheckUnique(redshift_hook, "artists", "artistid"),
+            CheckNotNull(redshift_hook, "artists", "artistid"),
+            CheckHasData(redshift_hook, "artists")
+        ]
+
+        songplays_tests = [
+            CheckUnique(redshift_hook, "songplays", "playid"),
+            CheckNotNull(redshift_hook, "songplays", "playid"),
+            CheckHasData(redshift_hook, "songplays")
+        ]
+
+        songs_tests = [
+            CheckUnique(redshift_hook, "songs", "songid"),
+            CheckNotNull(redshift_hook, "songs", "songid"),
+            CheckHasData(redshift_hook, "songs")
+        ]
+
+        users_tests = [
+            CheckUnique(redshift_hook, "users", "userid"),
+            CheckNotNull(redshift_hook, "users", "userid"),
+            CheckHasData(redshift_hook, "users")
+        ]
+
+        time_tests = [
+            CheckUnique(redshift_hook, "time", "start_time"),
+            CheckNotNull(redshift_hook, "time", "start_time"),
+            CheckHasData(redshift_hook, "time")
+        ]
+
+        artists_results = [(i.test_name, i.execute()) for i in artists_tests]
+        songplays_results = [(i.test_name, i.execute()) for i in songplays_tests]
+        songs_results = [(i.test_name, i.execute()) for i in songs_tests]
+        users_results = [(i.test_name, i.execute()) for i in users_tests]
+        time_results = [(i.test_name, i.execute()) for i in time_tests]
+
+
+        all_tests_results = artists_results + songplays_results + songs_results + users_results + time_results
+        
+        failed_tests = [test for test, result in all_tests_results if result == 'failed']
+
+        if any(failed_tests):
+            logger.error(f"Data quality check failed: {', '.join(failed_tests)}") 
+            raise ValueError("Data quality check failed")
         else:
-            logger.info("Data quality check passed.")
+            logger.info("Data quality check passed")           )
 
